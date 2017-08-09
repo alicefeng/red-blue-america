3ce573bfde4f95d084010317e3adb07539290ae5
Alice Feng <feng.a.g@gmail.com>
2017-08-08 22:24:41 -0400

    Modify layout of the nodes

--- js/isolate_force.js
+++ js/isolate_force.js
@@ -7,23 +7,22 @@ var color_scale = d3.scaleOrdinal()
   .domain(['D', 'R', 'I', 'Multi', 'Vacant', 'Third'])
   .range(['#64b5f6', '#e57373', '#90a4ae', '#9575cd', , '#fff176']);
 
-d3.json('data/al_upper_houses.json', function(error, data) {
+d3.json('data/vt_upper_houses.json', function(error, data) {
   
   data.forEach(function(d) {
     d.id = +d.id;
     d.party = d.party;
   });
 
-  var simulation = d3.forceSimulation(data)
-      .force("y", d3.forceY())
-      .force("democrat", isolate(d3.forceX(-width / 6), function(d) { return d.party === "D"; }))
-      .force("republican", isolate(d3.forceX(width / 6), function(d) { return d.party === "R"; }))
-      .force("independent", isolate(d3.forceX(width / 3), function(d) { return d.party === "I"; }))  // modify these lines to rearrange layout
-      .force("multi-party", isolate(d3.forceX(width / 6), function(d) { return d.party === "Multi"; }))
-      .force("vacant", isolate(d3.forceX(width / 6), function(d) { return d.party === "Vacant"; }))
-      .force("third", isolate(d3.forceX(width / 6), function(d) { return d.party === "Third"; }))
-      .force("charge", d3.forceManyBody().strength(-10))
-      .on("tick", ticked);
+  function drawNode(d) {
+    context.beginPath();
+    context.moveTo(d.x + 3, d.y);
+    context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+    context.fillStyle = color_scale(d.party);  //add stroke for vacant seats
+    context.fill();
+    context.strokeStyle = d.party === 'Vacant' ? '#7a7a7a' : '#F6F6F6';
+    context.stroke();
+  }
 
   function ticked() {
     context.clearRect(0, 0, width, height);
@@ -33,17 +32,20 @@ d3.json('data/al_upper_houses.json', function(error, data) {
     context.restore();
   }
 
-  function drawNode(d) {
-    context.beginPath();
-    context.moveTo(d.x + 3, d.y);
-    context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
-    context.fillStyle = color_scale(d.party);  //add stroke for vacant seats
-    context.fill();
-  }
-
   function isolate(force, filter) {
     var initialize = force.initialize;
     force.initialize = function() { initialize.call(force, data.filter(filter)); };
     return force;
   }
+
+  var simulation = d3.forceSimulation(data)
+      .force("y", d3.forceY())  // controls vertical displacement of the nodes
+      .force("democrat", isolate(d3.forceX(-width / 20), function(d) { return d.party === "D"; })) // modify these lines to rearrange horizontal layout
+      .force("republican", isolate(d3.forceX(width / 20), function(d) { return d.party === "R"; }))
+      .force("independent", isolate(d3.forceX(width / 40), function(d) { return d.party === "I"; }))  
+      .force("multi-party", isolate(d3.forceX(width / 6), function(d) { return d.party === "Multi"; }))
+      .force("vacant", isolate(d3.forceX(width / 6), function(d) { return d.party === "Vacant"; }))
+      .force("third", isolate(d3.forceX(-width / 40), function(d) { return d.party === "Third"; }))
+      .force("charge", d3.forceManyBody().strength(-5))  // controls how spread out nodes in the same group are from each other
+      .on("tick", ticked);
 });
